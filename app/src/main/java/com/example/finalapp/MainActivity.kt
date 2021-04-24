e1b361dedec656c3b3a0ea8106b6a445fffbeae3
DonDon2121 <81027157+DonDon2121@users.noreply.github.com>
2021-04-23 23:39:44 -0500

    dc

--- app/src/main/java/com/example/finalapp/MainActivity.kt
+++ app/src/main/java/com/example/finalapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.finalapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle//Donava
 // Donavan Cochran
+// Excel academy of tat kwon doe app
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
